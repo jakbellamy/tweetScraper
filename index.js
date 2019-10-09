5882abc589aaebfd7b15a4d58932a8eb9e9f42d2
Jakob Bellamy <jakbellamy@gmail.com>
2019-10-08 22:00:55 -0500

    trying to get handle element from request. just installed body-parser

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser'
 import {getUserTweets} from './lib/scraper';
 
 const app = express();
+  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.json())
 const PORT = 8060;
+const router = express.Router()
 
 app.get('/', (req, res) => {
     res.send('TweetScraper is live!');
 });
+app.route('/user_scraper')
+  .post(function (req, res, next) {
+    console.log(req);
+    res.send('successful query')
+  });
 
 app.listen(PORT, () => console.log(`TweetScraper listening on port ${PORT}!`));
 
