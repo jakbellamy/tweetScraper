481456e1b281ad015b5bbd9e12ce405e81cd32ed
Jakob Bellamy <jakbellamy@gmail.com>
2019-10-08 23:58:59 -0500

    showing the scraper working

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser'
 
 import {getUserTweets} from './lib/scraper';
+//change dril to whatever handle then check your console
+getUserTweets('dril')
+// const app = express()
+//   .use(bodyParser.urlencoded({extended: true}))
+//   .use(bodyParser.json());
+// const PORT = 8060;
+// const router = express.Router()
 
+// app.get('/', (req, res) => {
+//     res.send('TweetScraper is live! Austin, bich');
+// });
+// app.route('/user_scraper')
+//   .post(async function (req, res, next) {
+//     console.log(req.body.handle)
+//     let product = getUserTweets(req.body.handle)
+//     console.log(product)
+//     res.send({tweets: product})
+//   });
 
-const app = express()
-  .use(bodyParser.urlencoded({extended: true}))
-  .use(bodyParser.json());
-const PORT = 8060;
-const router = express.Router()
-
-app.get('/', (req, res) => {
-    res.send('TweetScraper is live! Austin, bich');
-});
-app.route('/user_scraper')
-  .post(async function (req, res, next) {
-    let product = await getUserTweets(req.body.handle)
-    res.send({tweets: product})
-  });
-
-app.listen(PORT, () => console.log(`TweetScraper listening on port ${PORT}!`));
-
-// getUserTweets('christisraisin')
+// app.listen(PORT, () => console.log(`TweetScraper listening on port ${PORT}!`));
