7e0a826aceac91405f588cebcf97acef50a1dd64
Jakob Bellamy <jakbellamy@gmail.com>
2019-10-08 23:25:26 -0500

    austin look at my index and scraper.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser'
 
 import {getUserTweets} from './lib/scraper';
-import {returnTweets} from './lib/controller';
+
 
 const app = express()
   .use(bodyParser.urlencoded({extended: true}))
@@ -14,7 +14,10 @@ app.get('/', (req, res) => {
     res.send('TweetScraper is live!');
 });
 app.route('/user_scraper')
-  .post(returnTweets);
+  .post(async function (req, res, next) {
+    let product = await getUserTweets('christisraisin')
+    res.send('successful query', product)
+  });
 
 app.listen(PORT, () => console.log(`TweetScraper listening on port ${PORT}!`));
 
